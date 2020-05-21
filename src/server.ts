5ed95fad98a5ff2bbe0e8d612ec0722681ad9e69
Bruno Girome <brunogirome@gmail.com>
2020-05-20 22:36:41 -0300

    EditorConfig, ESLint and Prettier configs

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World!' });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🧙‍♂️ Server started on port 3333.');
