0d5f47313f2cc60aa697e490e1e14cd120cc8c90
Bruno Girome <brunogirome@gmail.com>
2020-05-25 18:30:39 -0300

    Implementation of bycript

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
-import { Router } from 'express';
+import { Router, response } from 'express';
 
 import CreateUserService from '../services/CreateUserService';
 
 const usersRouter = Router();
+
+interface ResponseUser {
+  name: string;
+  email: string;
+  password?: string;
+}
 
 usersRouter.post('/', async (req, res) => {
   try {
@@ -16,7 +22,11 @@ usersRouter.post('/', async (req, res) => {
       password,
     });
 
-    return res.json(user);
+    const responseUser: ResponseUser = { ...user };
+
+    delete responseUser.password;
+
+    return res.json(responseUser);
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }
