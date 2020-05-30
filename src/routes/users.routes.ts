0c80d7855d64c4bf2f6afd663146536581b72ada
Bruno Girome <brunogirome@gmail.com>
2020-05-30 18:29:06 -0300

    User avatar upload

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import ensureAthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 interface NoPasswordUser {
   name: string;
   email: string;
   password?: string;
+  avatar: string;
 }
 
 usersRouter.post('/', async (req, res) => {
@@ -31,5 +37,27 @@ usersRouter.post('/', async (req, res) => {
     return res.status(400).json({ error: error.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user: NoPasswordUser = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  },
+);
 
 export default usersRouter;
