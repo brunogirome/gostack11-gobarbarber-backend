85801d3e59a496a495109b164f19bbc604177779
Bruno Girome <brunogirome@gmail.com>
2020-07-12 20:13:20 -0300

    Creation of a test that coverage 100% of CreateAppointmentService code

--- src/modules/appointments/services/CreateAppointmentServce.spec.ts
+++ src/modules/appointments/services/CreateAppointmentServce.spec.ts
@@ -1,5 +1,6 @@
 import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import AppError from '@shared/errors/AppError';
 
 describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
@@ -17,7 +18,24 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('123123123');
   });
 
-  // it('should not be able to create tow appointments on the same time/date', () => {
+  it('should not be able to create tow appointments on the same time/date', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
 
-  // });
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123123',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
