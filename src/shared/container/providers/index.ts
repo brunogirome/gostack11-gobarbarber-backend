811d796ed3c83a2751ba7f736747e4bc5cd73405
Bruno Girome <brunogirome@gmail.com>
2020-07-29 23:04:15 -0300

    Configuration of amazon SES

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,4 +1,5 @@
 import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
 
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
@@ -8,6 +9,7 @@ import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementatio
 
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherialMailProvider from './MailProvider/implementations/EtherialMailProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
@@ -21,5 +23,7 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherialMailProvider),
+  mailConfig.driver === 'etherial'
+    ? container.resolve(EtherialMailProvider)
+    : container.resolve(SESMailProvider),
 );
